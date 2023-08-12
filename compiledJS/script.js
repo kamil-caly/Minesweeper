60388e5ac6eebe023f582a5b04fa2ae702f51d17
Kamil Cały <kamil.caly@vp.pl>
2023-08-12 18:49:29 +0200

    mine collision + reveal field with any value

--- compiledJS/script.js
+++ compiledJS/script.js
@@ -1,9 +1,21 @@
 import { generateBoard } from "./generateBoard.js";
+const directions = {
+    UP: "up",
+    RIGHT_UP: "rightUp",
+    RIGHT: "right",
+    RIGHT_DOWN: "rightDown",
+    DOWN: "down",
+    LEFT_DOWN: "leftDown",
+    LEFT: "left",
+    LEFT_UP: "leftUp"
+};
 const timerCounter = document.getElementsByClassName("timerCounter")[0];
 const boardHTML = document.getElementsByClassName("board")[0];
+const minesCounter = document.getElementsByClassName("minesCounter")[0];
 const boardWidth = getComputedStyle(document.documentElement).getPropertyValue('--board-width');
 const boardHeight = getComputedStyle(document.documentElement).getPropertyValue('--board-height');
 const minesCount = 10;
+const generatedBoard = generateBoard(Number(boardWidth), Number(boardHeight), minesCount);
 const enableTimer = () => {
     const incrementTimer = () => {
         let content = (Number(timerCounter.textContent) + 1).toString();
@@ -19,16 +31,82 @@ const enableTimer = () => {
     };
     setInterval(incrementTimer, 1000);
 };
+const rightClick = (event) => {
+    event.preventDefault();
+    const clickedElement = document.getElementById(event.target['id']);
+    if (clickedElement.classList.contains('flagField')) {
+        clickedElement.classList.remove('flagField');
+        minesCounter.textContent = (Number(minesCounter.textContent) + 1).toString();
+    }
+    else if (Number(minesCounter.textContent) > 0) {
+        clickedElement.classList.add('flagField');
+        minesCounter.textContent = (Number(minesCounter.textContent) - 1).toString();
+    }
+    console.log(clickedElement);
+};
+const leftClick = (event) => {
+    const clickedElement = document.getElementById(event.target['id']);
+    if (clickedElement.classList.contains('flagField') || clickedElement.classList.length > 1)
+        return;
+    const [x, y] = clickedElement.id.split('-');
+    const [posX, posY] = [Number(x), Number(y)];
+    const isMineHit = generatedBoard.find(el => el.x === posX && el.y === posY && el.content === 'mine');
+    if (isMineHit) {
+        generatedBoard.forEach(el => {
+            if (el.content === 'mine') {
+                const currentMineElement = document.getElementById(el.x + "-" + el.y);
+                if (currentMineElement) {
+                    currentMineElement.classList.add('mineField');
+                    currentMineElement.innerHTML = '⨂';
+                }
+            }
+        });
+        setTimeout(gameOver, 50);
+    }
+    else {
+        revealField(clickedElement, posX, posY);
+    }
+};
+// recursive function
+const revealField = (clickedElement, posX, posY) => {
+    if (!clickedElement)
+        return;
+    const genBoardCurrentElem = generatedBoard.find(el => el.x === posX && el.y == posY);
+    if ((genBoardCurrentElem === null || genBoardCurrentElem === void 0 ? void 0 : genBoardCurrentElem.content) !== '') {
+        clickedElement.classList.add('visibleField');
+        clickedElement.textContent = genBoardCurrentElem.content;
+        return;
+    }
+    else {
+        clickedElement.classList.add('visibleField');
+    }
+    Object.values(directions).forEach(dir => {
+        console.log('dir: ', dir);
+        switch (dir) {
+            case directions.UP:
+                revealField(document.getElementById(posX + '-' + (posY - 1)), posX, posY - 1);
+                console.log("elem: ", document.getElementById(posX + '-' + (posY - 1)));
+                break;
+            default:
+                break;
+        }
+    });
+};
+const gameOver = () => {
+    alert(`Game Over!`);
+    location.reload();
+};
 const setHTMLBoard = (genBoard) => {
     genBoard.forEach(el => {
         const field = document.createElement('div');
         field.id = el.x + "-" + el.y;
-        field.classList.add('field');
+        field.classList.add('hideField');
+        field.addEventListener('contextmenu', rightClick);
+        field.addEventListener('click', leftClick);
         boardHTML.appendChild(field);
     });
 };
 const main = () => {
-    const generatedBoard = generateBoard(Number(boardWidth), Number(boardHeight), minesCount);
     setHTMLBoard(generatedBoard);
     enableTimer();
 };
--- script.ts
+++ script.ts
@@ -1,11 +1,26 @@
 import { boardContent, generateBoard } from "./generateBoard.js";
 
+const directions = {
+    UP: "up",
+    RIGHT_UP: "rightUp",
+    RIGHT: "right",
+    RIGHT_DOWN: "rightDown",
+    DOWN: "down",
+    LEFT_DOWN: "leftDown",
+    LEFT: "left",
+    LEFT_UP: "leftUp"
+}
+
+
 const timerCounter = document.getElementsByClassName("timerCounter")[0];
 const boardHTML = document.getElementsByClassName("board")[0];
+const minesCounter = document.getElementsByClassName("minesCounter")[0];
 
 const boardWidth = getComputedStyle(document.documentElement).getPropertyValue('--board-width');
 const boardHeight = getComputedStyle(document.documentElement).getPropertyValue('--board-height');
 const minesCount: number = 10;
+
+const generatedBoard = generateBoard(Number(boardWidth), Number(boardHeight), minesCount);
 
 const enableTimer = (): void => {
     const incrementTimer = (): void => {
@@ -22,17 +37,90 @@ const enableTimer = (): void => {
     setInterval(incrementTimer, 1000);
 }
 
+const rightClick = (event: MouseEvent): void => {
+    event.preventDefault();
+    const clickedElement = document.getElementById(event.target['id']);
+
+    if(clickedElement.classList.contains('flagField')) {
+        clickedElement.classList.remove('flagField');
+        minesCounter.textContent = (Number(minesCounter.textContent) + 1).toString();
+    } else if(Number(minesCounter.textContent) > 0) {
+        clickedElement.classList.add('flagField');
+        minesCounter.textContent = (Number(minesCounter.textContent) - 1).toString();
+    }
+    
+    console.log(clickedElement);
+}
+
+const leftClick = (event: MouseEvent): void => {
+    const clickedElement: HTMLElement = document.getElementById(event.target['id']);
+    if(clickedElement.classList.contains('flagField') || clickedElement.classList.length > 1)
+        return;
+
+    const [x, y]: string[] = clickedElement.id.split('-');
+    const [posX, posY]: number[] = [Number(x), Number(y)];
+    const isMineHit = generatedBoard.find(el => el.x === posX && el.y === posY && el.content === 'mine');
+    
+    if(isMineHit) {
+        generatedBoard.forEach(el => {
+            if(el.content === 'mine') {
+               const currentMineElement = document.getElementById(el.x + "-" + el.y);
+               if(currentMineElement) {
+                    currentMineElement.classList.add('mineField');
+                    currentMineElement.innerHTML = '⨂';
+               }
+            }
+        });
+        setTimeout(gameOver, 50);
+    } else {
+        revealField(clickedElement, posX, posY);
+    }
+}
+
+// recursive function
+const revealField = (clickedElement: HTMLElement, posX: number, posY: number): void => {
+    if(!clickedElement)
+        return;
+
+    const genBoardCurrentElem = generatedBoard.find(el => el.x === posX && el.y == posY);
+    if(genBoardCurrentElem?.content !== '') {
+        clickedElement.classList.add('visibleField');
+        clickedElement.textContent = genBoardCurrentElem.content as string;
+        return;
+    } else {
+        clickedElement.classList.add('visibleField');
+    }
+
+    Object.values(directions).forEach(dir => {
+        console.log('dir: ', dir);
+        switch (dir) {
+            case directions.UP:
+                    revealField(document.getElementById(posX + '-' + (posY - 1)), posX, posY - 1);
+                    console.log("elem: ", document.getElementById(posX + '-' + (posY - 1)))
+                break;
+            default:
+                break;
+        }
+    });
+}
+
+const gameOver = () => {
+    alert(`Game Over!`);
+    location.reload();
+}
+
 const setHTMLBoard = (genBoard: boardContent[]): void => {
     genBoard.forEach(el => {
         const field = document.createElement('div');
         field.id = el.x + "-" + el.y;
-        field.classList.add('field');
+        field.classList.add('hideField');
+        field.addEventListener('contextmenu', rightClick);
+        field.addEventListener('click', leftClick);
         boardHTML.appendChild(field);
     });
 }
 
 const main = () => {
-    const generatedBoard = generateBoard(Number(boardWidth), Number(boardHeight), minesCount);
     setHTMLBoard(generatedBoard);
     enableTimer();
 }
